6c8f6b3e95e94ccbe5f34d45cb96be492203ce6d
Anastasiia-Ciloci <anastasiiaciloci@gmail.com>
2022-03-17 23:00:39 -0400

    updated files

--- index.js
+++ index.js
@@ -2,47 +2,96 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-//const generateMarkdown = require("./utils/generateMarkdown");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
 
+// TODO: Create an array of questions for user input
+//const questions = [];
 inquirer
   .prompt([
     {
       type: "input",
-      message: "What is you project name?",
+      message: "Enter the project title:",
       name: "title",
     },
     {
       type: "input",
-      message: "Description of the project",
+      message: "Description of the project:",
       name: "description",
     },
     {
+      type: "confirm",
+      message: "Table of Contents",
+      name: "tableOfContents",
+      default: [Boolean],
+      //maybe list
+    },
+    {
       type: "input",
-      message: "Usage information",
-      name: "description",
+      message: "Installation:",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Usage information:",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "What license did you use?",
+      name: "license",
+      choices: ["The MIT License", "Apache license", "The BSD License", "None"],
+    },
+    {
+      type: "input",
+      message: "Contributing:",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "Tests:",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Questions:",
+      name: "questions",
+    },
+    {
+      type: "input",
+      message: "GitHub user name:",
+      name: "userName",
+    },
+    {
+      type: "input",
+      message: "GitHub link:",
+      name: "gitHubLink",
+    },
+    {
+      type: "input",
+      message: "Enter your email:",
+      name: "email",
     },
   ])
-  .then(
-    (response) =>
-      // TODO: Create an array of questions for user input
-      //const questions = [];
+  .then((answers) => {
+    //console.log(answers);
+    writeToFile("README.md", generateMarkdown(answers));
+  });
 
-      // TODO: Create a function to write README file
-      function writeToFile(fileName, data) {
-        fs.writeFile(
-          "./dist/log.json",
-          JSON.stringify(answers, null, 2),
-          (err) => {
-            err
-              ? console.error(err)
-              : console.log("Success! Here is your README");
-          }
-        );
-      }
-  );
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, (err) => {
+    err ? console.error(err) : console.log("Success! Here is your README");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  // .then(function (answers) {
+  //   writeToFile("./dist/README.md", generateMarkdown(answers));
+  // });
+  //generateMarkdown(data);
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,34 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}`;
+  const github = `https://github.com/${data.userName}`;
+  return `
+  # ${data.title} 
+  https://githun.com/${data.userName}/${data.title}
+  ## Description
+  ${data.description}
+  ## Table of contents
+  - [Instalation](#installation)
+  - [License](#license)
+  - [Contributing](#contribution)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  In order to use this app, ${data.usage}
+  ## License
+  This project is licensed under the ${data.license} license.
+  ![GitHub License](https://img.shields.io/badge/license-MIT-green.svg)
+  ${data.license}
+  ## Contributing
+  Contributors: ${data.contributing}
+  ## Tests
+  To run this test you need${data.tests}
+  ## Questions
+  ${data.questions}
+  If you have any question about this project, you can reach me out by ${data.email}.
+  `;
 }
-
+//${data.userName} ${data.gitHubLink}
 module.exports = generateMarkdown;
